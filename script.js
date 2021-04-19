8e231652fe25744a1a626a34f9e3df62e631f6d9
rilith <bludoftenchiefs@protonmail.com>
2021-04-19 16:00:31 -0500

    finishing touches on script.js, index.html, style.css

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const h2 = document.createElement('h2');
 h2.textContent = "remember we all must die";
 
-document.querySelector('body').appendChild(h2);
+document.querySelector('.h2-div').appendChild(h2);
+
+const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('We all have an appointment with Death');
+}); 
